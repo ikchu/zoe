cb00519f046937faa5973cd944faa0bc629ffe41
Ikaia Chu <ikaia_chu@outlook.com>
2021-05-29 20:23:08 -1000

    feat (django): add Favicon

--- django/zoe/urls.py
+++ django/zoe/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 from users import rest_views as user_rest_views
 from feed import views as feed_views
@@ -55,6 +57,7 @@ rest_urls = [
 ]
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('icons/favicon.ico'))),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
