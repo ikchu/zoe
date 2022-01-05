d738ab1de9cef12842dba9721e2781c9ec885ba9
kphan20 <knpmths@gmail.com>
2022-01-05 16:05:59 -0500

    feat: created Event and Theme serializers

---
+++ django/events/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from events.models import Theme, Event
+from users.serializers import UserSerializer
+
+class ThemeSerializer(serializers.HyperlinkedModelSerializer):
+    interested_users = UserSerializer(read_only=True, many=True)
+    class Meta:
+        model = Theme
+        fields = ['title','interested_users']
+    
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    interested_users = UserSerializer(read_only=True, many=True)
+    committed_users = UserSerializer(read_only=True, many=True)
+    themes = ThemeSerializer(read_only=True, many=True)
+    class Meta:
+        model = Event
+        fields = ['title', 'desc', 'creator', 'date_created', 'themes', 'long', 
+                  'lat', 'interested_users', 'committed_users', 'capacity',
+                  'price', 'is_recurring', 'recurring_duration',
+                  'visibility', 'image']
+        # the creator should be added automatically through viewset method
+        # long and lat coordinates will have to be sorted out later
+        read_only_fields = ['creator', 'long', 'lat']
