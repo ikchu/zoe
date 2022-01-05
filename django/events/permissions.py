38d9439014a0a128377400f1e14695b88c1d5c15
kphan20 <knpmths@gmail.com>
2022-01-05 16:06:23 -0500

    feat: created permissions for events

---
+++ django/events/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+
+class EventPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+    def has_object_permission(self, request, view, obj):
+        return request.method in permissions.SAFE_METHODS or obj == request.user
